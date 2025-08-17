36d41808fc27f40b0827956a3d10872f0ae24c4e
张孟伟 <1436373010@qq.com>
2025-08-18 01:06:27 +0800

    重新构造

--- silence-content-service/src/main/java/com/old/silence/content/api/ContentIndexResource.java
+++ silence-content-service/src/main/java/com/old/silence/content/api/ContentIndexResource.java
@@ -7,8 +7,6 @@ import java.util.Map;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.SearchPage;
-import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
-import org.springframework.data.elasticsearch.core.query.CriteriaQueryBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
