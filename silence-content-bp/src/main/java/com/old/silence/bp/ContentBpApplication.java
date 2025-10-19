a804a2432ec79a59d43b3d9117dd8d604739bb21
moryzang <oncwnuFwc-eWOe1YCSOUhEZOjYFY@git.weixin.qq.com>
2025-10-19 23:11:58 +0800

    test data

--- silence-content-bp/src/main/java/com/old/silence/bp/ContentBpApplication.java
+++ silence-content-bp/src/main/java/com/old/silence/bp/ContentBpApplication.java
@@ -1,12 +1,17 @@
 package com.old.silence.bp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import com.old.silence.job.client.starter.EnableSilenceJob;
 
 /**
  * @author moryzang
  */
-
-//@EnableFeignClients({"com.old.silence.content.api"})
+@EnableFeignClients({"com.old.silence.content.api"})
+@EnableSilenceJob
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ContentBpApplication {
 
 
